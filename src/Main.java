bb1ecab67c058fdc01a6e0b7c2e1503487fd4d0c
kaevtimov <39729854+kaevtimov@users.noreply.github.com>
2018-07-08 13:17:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
 
         //pravim si BOSS, sklad, case, admins, customer, supplier
-<<<<<<< HEAD
 
         Boss boss = new Boss("Ivan Ivanov", PersonalType.BOSS, "BG", "info");
         Administrator admin = new Administrator("Stefcho", 2500, PersonalType.ADMINISTRATOR, "BG", "info");
@@ -16,48 +15,8 @@ public class Main {
         Customer customer2 = new Customer("Harry Kane", 6000, PersonalType.CUSTOMER, "BG", "adress", CustomerType.PARTTIME);
         Supplier supplier = new Supplier(PersonalType.SUPPLIER);
 
-        Warehouse warehouse = new Warehouse("TheHulk");            // KE
-        Case warehouseCase = new Case(2500000,0);        // KE
-
-        boss.investWarehouseAndCase();
-
-//        System.out.println(customer);
-//        System.out.println(customer2);
-//        System.out.println(admin);
-//        System.out.println(boss);
-
-
-        warehouseCase.checkBalance();
-
-        //podrejdame kataloga
-        boss.setUpCatalog(warehouse);
-
-
-        // zarejdame sklada s produkti
-        warehouse.checkProductsQuantity(admin, supplier, warehouseCase);
-        System.out.println();
-//        warehouse.checkProductsQuantity(admin, supplier, warehouseCase);
-//        System.out.println();
-        warehouseCase.checkBalance();
-//        System.out.println();
-        //System.out.println(warehouseCase.getOutcome());
-
-
-
-        // customera otiva i kazva kakvo tursi, a admina proverqva po ime i po nalichnost dali ima v sklada
-        // posle customera constructCustomerRequests();   kato si adva v zaqvkata
-        warehouse.searchInWarehouse(customer, admin, warehouseCase);     // chisloto  e za broq na poruchkite
-        System.out.println();
-=======
-
-        Boss boss = new Boss("Ivan Ivanov", PersonalType.BOSS, "BG", "info");
-        Administrator admin = new Administrator("Stefcho", 2500, PersonalType.ADMINISTRATOR, "BG", "info");
-        Customer customer = new Customer("Pencho Tenev", 600, PersonalType.CUSTOMER, "BG", "adress", CustomerType.LONGTIME);
-        Customer customer2 = new Customer("Harry Kane", 6000, PersonalType.CUSTOMER, "BG", "adress", CustomerType.PARTTIME);
-        Supplier supplier = new Supplier(PersonalType.SUPPLIER);
-
-        Warehouse warehouse = new Warehouse("TheHulk");            // KE
-        Case warehouseCase = new Case(2500000,0);        // KE
+        Warehouse warehouse = new Warehouse("TheHulk");          
+        Case warehouseCase = new Case(2500000,0);   
 
         boss.investWarehouseAndCase();
 
@@ -88,7 +47,6 @@ public class Main {
         // posle customera constructCustomerRequests();   kato si adva v zaqvkata
         warehouse.searchInWarehouse(customer, admin, warehouseCase);     // chisloto  e za broq na poruchkite
 //        System.out.println();
->>>>>>> 9f36506778bd373359cb26239c84b316ecf158bd
 //        warehouse.searchInWarehouse(customer2, admin, warehouseCase);   // probvame s vtori customer
 //        System.out.println();
 
